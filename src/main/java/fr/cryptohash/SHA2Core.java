bc5192469a6ae8e8a8d5b90608da3749e7e77ccf
James Northrup <northrup.james@gmail.com>
2013-03-03 19:24:11 -0800

    intellij glues in the aparapi. see below stacktrace
    /opt/jdk1.7.0_10/bin/java -Djava.library.path=/home/jim/work/aparapi/com.amd.aparapi.jni/dist -Dfile.encoding=UTF-8 -classpath
    [...]:/home/jim/work/aparapi/com.amd.aparapi/dist/aparapi.jar fr.cryptohash.Speed SHA256
    Speed test: SHA-256
    Exception in thread "main" java.lang.IllegalStateException: Cannot send null refs to kernel, reverting to java
    	at com.amd.aparapi.KernelRunner.updateKernelArrayRefs(KernelRunner.java:1223)
    	at com.amd.aparapi.KernelRunner.executeOpenCL(KernelRunner.java:1306)
    	at com.amd.aparapi.KernelRunner.execute(KernelRunner.java:1603)
    	at com.amd.aparapi.Kernel.execute(Kernel.java:1774)
    	at com.amd.aparapi.Kernel.execute(Kernel.java:1705)
    	at com.amd.aparapi.Kernel.execute(Kernel.java:1675)
    	at fr.cryptohash.SHA2Core.processBlock(SHA2Core.java:146)
    	at fr.cryptohash.DigestEngine.update(DigestEngine.java:180)
    	at fr.cryptohash.DigestEngine.update(DigestEngine.java:164)
    	at fr.cryptohash.MDHelper.makeMDPadding(MDHelper.java:110)
    	at fr.cryptohash.SHA2Core.doPadding(SHA2Core.java:123)
    	at fr.cryptohash.DigestEngine.digest(DigestEngine.java:131)
    	at fr.cryptohash.Speed.speedUnit(Speed.java:300)
    	at fr.cryptohash.Speed.speed(Speed.java:250)
    	at fr.cryptohash.Speed.main(Speed.java:210)
    
    Process finished with exit code 1

--- src/main/java/fr/cryptohash/SHA2Core.java
+++ src/main/java/fr/cryptohash/SHA2Core.java
@@ -43,27 +43,7 @@ import com.amd.aparapi.Range;
 
 abstract class SHA2Core extends MDHelper {
 
-    /**
-     * private special values.
-     */
-    private static final int[] K = {
-            0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
-            0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
-            0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
-            0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
-            0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
-            0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
-            0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
-            0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
-            0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
-            0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
-            0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
-            0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
-            0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
-            0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
-            0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
-            0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
-    };
+
     private int[] currentVal = null;
     private int[] W = null;
 
@@ -158,38 +138,65 @@ abstract class SHA2Core extends MDHelper {
     /**
      * @see DigestEngine
      */
-    protected void processBlock(final byte... data) {
-        Kernel runnable = new Kernel() {
-            public void run() {
-                int H = currentVal[7];
-                int G = currentVal[6];
-                int F = currentVal[5];
-                int E = currentVal[4];
-                int D = currentVal[3];
-                int C = currentVal[2];
-                int B = currentVal[1];
-                int A = currentVal[0];
+    protected void processBlock(final byte[] input) {
+                                                  final byte[]data=input;
+        final int[] cv=this.currentVal;
+        final int[]work=this.W;
 
-                int T1;
-                int T2;
+        new Kernel() {
+            /**
+             * private special values.
+             */
+            private   final int[] K = {
+                    0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
+                    0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+                    0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+                    0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+                    0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
+                    0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+                    0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
+                    0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+                    0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+                    0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+                    0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
+                    0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+                    0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
+                    0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+                    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+                    0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
+            };
+
+            public void run() {
+                int H = cv[7];
+                int G = cv[6];
+                int F = cv[5];
+                int E = cv[4];
+                int D = cv[3];
+                int C = cv[2];
+                int B = cv[1];
+                int A = cv[0];
+
                 for (int i = 0; i < 16; i++) {
                     int off = i << 2;
-                    W[i] = (data[off++] & 0xFF) << 24
+                    work[i] = (data[off++] & 0xFF) << 24
                             | (data[off++] & 0xFF) << 16
                             | (data[off++] & 0xFF) << 8
                             | data[off] & 0xFF;
                 }
+
+                int T2 = 0;
+                int T1 = 0;
                 for (int i = 16; i < 64; i++) {
-                    T1 = W[i - 2];
-                    T2 = W[i - 15];
-                    W[i] = r3(T1 << 15 | T1 >>> 17, T1 >>> 19 | T1 << 13, T1 >>> 10)
-                            + W[i - 7]
+                    T1 = work[i - 2];
+                    T2 = work[i - 15];
+                    work[i] = r3(T1 << 15 | T1 >>> 17, T1 >>> 19 | T1 << 13, T1 >>> 10)
+                            + work[i - 7]
                             + r3(T2 << 25 | T2 >>> 7, T2 >>> 18 | T2 << 14, T2 >>> 3)
-                            + W[i - 16];
+                            + work[i - 16];
                 }
                 for (int i = 0; i < 64; i++) {
                     T1 = H + r3(E << 26 | E >>> 6, E >>> 11 | E << 21, E << 7 | E >>> 25) + (F & E ^ G & ~E)
-                            + K[i] + W[i];
+                            + K[i] + work[i];
                     T2 = r3(A >>> 2 | A << 30, A << 19 | A >>> 13, A >>> 22 | A << 10)
                             + r3(A & B, A & C, B & C);
                     //todo: long rot
@@ -202,14 +209,14 @@ abstract class SHA2Core extends MDHelper {
                     B = A;
                     A = T1 + T2;
                 }
-                currentVal[7] += H;
-                currentVal[6] += G;
-                currentVal[5] += F;
-                currentVal[4] += E;
-                currentVal[3] += D;
-                currentVal[2] += C;
-                currentVal[1] += B;
-                currentVal[0] += A;
+                cv[7] += H;
+                cv[6] += G;
+                cv[5] += F;
+                cv[4] += E;
+                cv[3] += D;
+                cv[2] += C;
+                cv[1] += B;
+                cv[0] += A;
             }
         }.execute(new Range(Device.best(), getBlockLength()));
 
